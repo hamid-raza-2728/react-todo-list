4dbf502b384d394ee1607af77994d71b24e6bd6b
MD Hamid Raza <shaikhhamidraza98710@gmail.com>
2026-09-20 18:12:44 +0530

    Fix import paths for components and styles

--- saveList/Src/index.js
+++ saveList/Src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
-import "./../public/styles.css";
+import App from "./Components/App";
+import "./../Public/style.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
